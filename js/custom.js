7d09314c865f1b7662e1e2a0fb7602def5c5b0f9
Gustavo Rodrigues <grodriguers@gmail.com>
2024-06-04 18:01:45 -0300

    fix: footer layout and smoth scroll

--- js/custom.js
+++ js/custom.js
@@ -18,3 +18,13 @@ function getCurrentYear() {
 }
 
 getCurrentYear();
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
